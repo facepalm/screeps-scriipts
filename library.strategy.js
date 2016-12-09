5564f67035231e8465d142dc134547911790129c
Brad <bbusse@gmail.com>
2016-12-08 22:43:13 -0500

    Adding stub strategy library

---
+++ library.strategy.js
@@ -0,0 +1,39 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('library.utility');
+ * mod.thing == 'a thing'; // true
+ */
+
+
+
+
+    
+var plantFlags = function (room) {
+    //given a room to set up, establishes flags denoting locations of future behavior
+    
+    //spawn position (?)
+    
+    //source points, one miner per source (inefficient at first, but quickly enough one creep will drain it)
+    
+    //storage can next to controller
+    
+    //building site expansions
+    
+    //walls
+    
+    //tower sites
+    
+        
+    };
+    
+var test = function () {};    
+    
+    
+    //strat.plantFlags = plantFlags;
+      
+module.exports.plantFlags = plantFlags;
+module.exports.test = test;
+    
